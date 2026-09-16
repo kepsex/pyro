ddfd57263d4d7871a915690c56962203013276aa
jeremiah. <lesmanansurya20@gmail.com>
2026-09-16 20:01:33 +0700

    Update __init__.py

--- pyrogram/types/bots_and_keyboards/__init__.py
+++ pyrogram/types/bots_and_keyboards/__init__.py
@@ -1,21 +1,3 @@
-#  Pyrogram - Telegram MTProto API Client Library for Python
-#  Copyright (C) 2017-present Dan <https://github.com/delivrance>
-#
-#  This file is part of Pyrogram.
-#
-#  Pyrogram is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU Lesser General Public License as published
-#  by the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  Pyrogram is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU Lesser General Public License for more details.
-#
-#  You should have received a copy of the GNU Lesser General Public License
-#  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
-
 from .bot_command import BotCommand
 from .bot_command_scope import BotCommandScope
 from .bot_command_scope_all_chat_administrators import BotCommandScopeAllChatAdministrators
@@ -32,6 +14,7 @@ from .game_high_score import GameHighScore
 from .inline_keyboard_button import InlineKeyboardButton
 from .inline_keyboard_markup import InlineKeyboardMarkup
 from .keyboard_button import KeyboardButton
+from .keyboard_button_style import KeyboardButtonStyle  # ← TAMBAHKAN INI
 from .login_url import LoginUrl
 from .menu_button import MenuButton
 from .menu_button_commands import MenuButtonCommands
@@ -50,6 +33,7 @@ __all__ = [
     "InlineKeyboardButton",
     "InlineKeyboardMarkup",
     "KeyboardButton",
+    "KeyboardButtonStyle",  # ← TAMBAHKAN INI
     "ReplyKeyboardMarkup",
     "ReplyKeyboardRemove",
     "LoginUrl",
